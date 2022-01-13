c4662df88db34be43d72a819152a0d0d4d5d6001
sim <git@sgougeon.fr>
2022-01-13 19:54:13 +0100

    Check redis for admin settings

--- lib/Settings/AdminSettings.php
+++ lib/Settings/AdminSettings.php
@@ -4,22 +4,55 @@ namespace OCA\UnifiedPushProvider\Settings;
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\Settings\ISettings;
 use OCP\IDBConnection;
+use OCP\IConfig;
 use OCP\IGroupManager;
 use OCP\IUserSession;
+use Redis;
 
 class AdminSettings implements ISettings {
 
+	private $config;
 	private $db;
 	private $userSession;
 	private $groupManager;
 
-	public function __construct(IDBConnection $db, IUserSession $userSession, IGroupManager $groupManager){
+	function checkRedis() {
+		$redis_db = 1;
+
+		if (!class_exists('Redis')) return "Redis is not installed.";
+
+		$redis_config = $this->config->getSystemValue("redis");
+		if (!is_array($redis_config)) return "Redis is not configured in nextcloud config.php.";
+		if (array_key_exists("dbindex", $redis_config)) $redis_db = $redis_config['dbindex'] + 1;
+
+		$redis = new Redis();
+		if (!$redis->connect($redis_config['host'], $redis_config['port'])) {
+			return "Cannot connect to Redis.";
+		}
+		if (array_key_exists('password', $redis_config) && $redis_config['password'] !== '') {
+			$redis->auth($redis_config['password']);
+		}
+		if(!$redis->select($redis_db)) {
+			return "Cannot select in Redis.";
+		}
+
+		return "";
+	}
+
+	public function __construct(IConfig $config, IDBConnection $db, IUserSession $userSession, IGroupManager $groupManager){
+		$this->config = $config;
 		$this->db = $db;
 		$this->userSession = $userSession;
 		$this->groupManager = $groupManager;
 	}
 
 	public function getForm() {
+		$parameters = array();
+		$redis_error = $this->checkRedis();
+		if ($redis_error !== "") {
+			$parameters['error'] = $redis_error;
+			return new TemplateResponse("uppush", "admin-redis-error", $parameters);
+		}
 		$query = $this->db->getQueryBuilder();
 		$query->select('*')
 			->from('uppush_config')
@@ -30,7 +63,6 @@ class AdminSettings implements ISettings {
 			$keepalive = $row['value'];
 		}
 		$result->closeCursor();
-		$parameters = array();
 		$parameters['keepalive'] = $keepalive;
 		return new TemplateResponse("uppush", 'admin-settings', $parameters);
 	}
---
+++ templates/admin-redis-error.php
@@ -0,0 +1,7 @@
+<div id="uppush-admin" class="section">
+<h2>UnifiedPush Provider Settings</h2>
+<br>
+<div>
+<b>You have an error with your redis server:</b><br>
+<?php echo $_['error']; ?>
+</div>
